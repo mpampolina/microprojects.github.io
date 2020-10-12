e02eaa5f406e7ab73989b1cfa1a03e9a382a2fc0
mpampolina <miguel.r.pampolina@gmail.com>
2020-10-11 17:43:48 -0700

    add slideshow functionality

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ var slideRight = document.getElementById("slideRight")
 
 sideMenu.addEventListener("mouseenter", menuPopout);
 sideMenu.addEventListener("mouseleave", menuPopback);
-
+slideRight.addEventListener("click", moveSlide);
+slideLeft.addEventListener("click", moveSlide)
 
 function menuPopout(event) {
     let navigation = document.querySelector("#navigation");
     let title = document.querySelector("#titleText");
     navigation.style.display = "block";
     title.style.display = "none";
-    console.log("popout")
 }
 
 function menuPopback(event) {
@@ -20,5 +20,49 @@ function menuPopback(event) {
     let title = document.querySelector("#titleText");
     navigation.style.display = "none";
     title.style.display = "block";
-    console.log("pop-in")
+}
+
+function moveSlide(event) {
+    let slideImages = Array.from(document.getElementsByClassName("slideImage"))
+    
+    // find the index of the current image
+    let currentBlockIndex= slideImages.findIndex(function(image){
+        if (image.style.display == "block") {
+            return image
+        }
+    })
+
+    // move forward the slide deck
+    if (event.target.id == "slideRight") {
+        let nextBlock;
+        if ((currentBlockIndex + 1) >= slideImages.length){
+            nextBlock = slideImages[0]
+        } else {
+            nextBlock = slideImages[currentBlockIndex + 1]
+        }   
+        slideImages.forEach(function(image){
+            if (image == nextBlock) {
+                image.style.display = "block";
+            } else {
+                image.style.display = "none";
+            }
+        })
+
+    // move backward the slide deck
+    } else if (event.target.id == "slideLeft") {
+        let prevBlock;
+        if ((currentBlockIndex - 1) < 0) {
+            prevBlock = slideImages[slideImages.length - 1];
+        } else {
+            prevBlock = slideImages[currentBlockIndex - 1];
+        }
+        slideImages.forEach(function(image){
+            if (image == prevBlock){
+                image.style.display = "block";
+            } else {
+                image.style.display = "none";
+            }
+        })
+    }
+    
 }
