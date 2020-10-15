8e195f458fee58742ac16d65fc0ee0d259536323
mpampolina <miguel.r.pampolina@gmail.com>
2020-10-14 21:45:24 -0700

    snake works with collision detection

--- snake/app.js
+++ snake/app.js
@@ -3,54 +3,80 @@
 2. Be able to control the snake
 3. The snake will move automatically after 1 second
 3. Have the snake grow by one cell after an apple has been eaten
-4. If the snake bumps into itself or into a wall it is game over. */
+4. If the snake bumps into itself or into a wall it is game over. 
+3+2+1*10 = 15
+4+1+2*10 = 25
+5+0+10 = 15
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
   const squares = document.querySelectorAll(".grid div");
+  console.log(squares);
   let snake = {
-    tailCoordinate: 0,
-    body: 3,
-    direction: 1,
+    Coordinates: [32, 31, 30],
+    Direction: [1, 1, 1],
   };
   const arrowKeys = { left: 37, up: 38, right: 39, down: 40 };
   const rowWidth = 10;
-  let iterations = 4;
+  let currentDirection = 1;
 
   function moveSnake() {
-    squares.forEach((square) => {
-      square.classList.remove("snake");
-    });
-    for (i = 0; i < snake.body; i++) {
-      squares[snake.tailCoordinate + i + snake.direction].className = "snake";
-    }
-    snake.tailCoordinate++;
-    iterations--;
-    if (iterations <= 0) {
+    squares.forEach((square) => square.classList.remove("snake"));
+    gameOver = collisionDetect(snake.Coordinates[0])
+    if (gameOver !== true) {
+      for (i = 0; i < snake.Coordinates.length; i++) {
+        snake.Coordinates[i] = snake.Coordinates[i] + snake.Direction[i];
+        squares[snake.Coordinates[i]].classList.add("snake");
+      }
+    } else {
+      alert("Game Over")
       clearInterval(timerID);
-      alert("done.")
     }
+    snake.Direction.pop();
+    snake.Direction.unshift(currentDirection);
+    console.log(currentDirection);
   }
 
-  function snakeTurn(snake) {}
+  function collisionDetect(headCoordinate) {
+    let gameOver = false
+    if (headCoordinate + currentDirection < 0) {
+      gameOver = true;
+    } else if (headCoordinate % 10 === 0) {
+      gameOver = true;
+    } else if (headCoordinate % 10 === 9) {
+      gameOver = true;
+    } else if (headCoordinate + currentDirection > 100) {
+      gameOver = true;
+    }
+    return gameOver;
+  }
 
   function controlSnake(event) {
+    console.log("hello");
     switch (event.keyCode) {
       case arrowKeys["left"]:
-        direction = -1;
+        if (snake.Direction[0] != 1) {
+          currentDirection = -1;
+        }
         break;
       case arrowKeys["up"]:
-        direction = -rowWidth;
+        if (snake.Direction[1] !== rowWidth) {
+          currentDirection = -rowWidth;
+        }
         break;
       case arrowKeys["right"]:
-        direction = 1;
+        if (snake.Direction[0] !== -1) {
+          currentDirection = 1;
+        }
         break;
       case arrowKeys["down"]:
-        direction = rowWidth;
+        if (snake.Direction[0] !== -rowWidth) {
+          currentDirection = rowWidth;
+        }
         break;
     }
   }
 
-  let timerID = setInterval(moveSnake, 1000)
-
-
+  document.addEventListener("keydown", controlSnake);
+  let timerID = setInterval(moveSnake, 500);
 });
