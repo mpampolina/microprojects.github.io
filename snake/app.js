b26238baa18f7e095e721c4705a2c006227ef754
mpampolina <miguel.r.pampolina@gmail.com>
2020-10-21 19:56:56 -0700

    single segment snake functions

--- snake/app.js
+++ snake/app.js
@@ -7,136 +7,84 @@
 */
 
 document.addEventListener("DOMContentLoaded", () => {
-  const squares = document.querySelectorAll(".grid div");
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const snakeSegment = {
+    x: 0,
+    y: 150,
+    w: 50,
+    h: 50,
+    dx: 2.5,
+    dy: 0,
+    nextdx: 2.5,
+    nextdy: 0,
+    speed: 2.5,
+  };
   const start = document.querySelector("#start");
-  const scoreboard = document.querySelector('#score');
+  const scoreboard = document.querySelector("#score");
   const arrowKeys = { left: 37, up: 38, right: 39, down: 40 };
-  // snake object
-  let snake;
-  // row width (in divs) of the game board
-  const rowWidth = 10;
-  // user inputted value that dictates the next direction instruction for the
-  // snake head
-  let nextCurrentDirection;
-  let timerID;
-  let score;
-
-  function gameStart() {
-    squares.forEach((square) => square.classList.remove("snake"));
-    squares.forEach((square) => square.classList.remove("apple"));
-    snake = {
-      // div coordinates for the snake's head, body, and tail respectively
-      Coordinates: [32, 31, 30],
-      // direction instructions for the snake's head, body and tail respectively
-      Direction: [1, 1, 1],
-      // direction instructions for the snake's trailing "ghost" tail
-      TailDirection: 1,
-    };
-    // the next direction instruction for the snake head is to move right
-    nextCurrentDirection = 1;
-    incrementScore(true);
-    generateApple();
-    document.addEventListener("keydown", controlSnake);
-    timerID = setInterval(moveSnake, 250);
-  }
-
-  function incrementScore(newGame) {
-    if (newGame) {
-      score = 0
-    } else {
-      score++;
-    }
-    scoreboard.textContent = score;
-  }
-
-  function moveSnake() {
-    [gameOver, apple] = collisionDetect(
-      snake.Coordinates[0],
-      snake.Direction[0]
-    );
-    snake.Coordinates.forEach((index) =>
-      squares[index].classList.remove("snake")
-    );
-    if (!gameOver) {
-      if (apple) {
-        // get the last snake segment and reverse the previous tail direction
-        // to determine where the new segment should be placed
-        let tailCoordinate =
-          snake.Coordinates[snake.Coordinates.length - 1] +
-          -1 * snake.TailDirection;
-        snake.Coordinates.push(tailCoordinate);
-        snake.Direction.push(snake.TailDirection);
-        incrementScore(false);
-      }
-      // move the snake coordinates by their direction and then draw the snake
-      for (i = 0; i < snake.Coordinates.length; i++) {
-        snake.Coordinates[i] = snake.Coordinates[i] + snake.Direction[i];
-        squares[snake.Coordinates[i]].classList.add("snake");
-      }
-    } else {
-      alert(`Game Over. Your final score was ${score}. Press start to play again!`);
-      squares.forEach((square) => square.classList.remove("apple"));
-      clearInterval(timerID);
-    }
-    // remove the last direction instruction and store it as the "ghost" value
-    snake.TailDirection = snake.Direction.pop();
-    // add the latest user inputted direction instruction
-    snake.Direction.unshift(nextCurrentDirection);
-  }
-
-  function collisionDetect(headCoordinate, currentDirection) {
-    let gameOver = false;
-    let apple = false;
-    let futureHead = headCoordinate + currentDirection;
-    if (
-      futureHead < 0 ||
-      (headCoordinate % rowWidth === 0 && currentDirection === -1) ||
-      (headCoordinate % rowWidth === rowWidth - 1 && currentDirection === 1) ||
-      futureHead > rowWidth * rowWidth ||
-      // check that the snake does not cross over itself
-      snake.Coordinates.indexOf(futureHead) !== -1
-    ) {
-      gameOver = true;
-    } else if (squares[futureHead].classList.contains("apple")) {
-      squares[futureHead].classList.remove("apple");
-      generateApple();
-      apple = true;
-    }
-    return [gameOver, apple];
-  }
-
-  function generateApple() {
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * Math.pow(rowWidth, 2));
-    } while (squares[randomIndex].classList.contains("snake"));
-    squares[randomIndex].classList.add("apple");
-  }
 
   function controlSnake(event) {
     switch (event.keyCode) {
       case arrowKeys["left"]:
-        if (snake.Direction[0] != 1) {
-          nextCurrentDirection = -1;
-        }
-        break;
-      case arrowKeys["up"]:
-        if (snake.Direction[1] !== rowWidth) {
-          nextCurrentDirection = -rowWidth;
+        if (snakeSegment.dx !== snakeSegment.speed) {
+          snakeSegment.nextdx = -snakeSegment.speed
+          snakeSegment.nextdy = 0
         }
         break;
       case arrowKeys["right"]:
-        if (snake.Direction[0] !== -1) {
-          nextCurrentDirection = 1;
+        if (snakeSegment.dx !== -snakeSegment.speed) {
+          snakeSegment.nextdx = snakeSegment.speed;
+          snakeSegment.nextdy = 0
+        }
+        break;
+      case arrowKeys["up"]:
+        if (snakeSegment.dy !== snakeSegment.speed) {
+          snakeSegment.nextdy = -snakeSegment.speed;
+          snakeSegment.nextdx = 0;
         }
         break;
       case arrowKeys["down"]:
-        if (snake.Direction[0] !== -rowWidth) {
-          nextCurrentDirection = rowWidth;
+        if (snakeSegment.dy !== -snakeSegment.speed) {
+          snakeSegment.nextdy = snakeSegment.speed;
+          snakeSegment.nextdx = 0;
         }
         break;
     }
   }
 
-  start.addEventListener("click", gameStart);
+  function drawSnakeSegment(x, y) {
+    ctx.beginPath();
+    ctx.rect(x, y, snakeSegment.w, snakeSegment.h);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+  }
+
+  function nextPosition() {
+    if ((Math.abs(snakeSegment.dx) == snakeSegment.speed && 
+      snakeSegment.x % snakeSegment.w === 0) ||
+      (Math.abs(snakeSegment.dy) == snakeSegment.speed && 
+        snakeSegment.y % snakeSegment.h === 0)
+      ) {
+      snakeSegment.dx = snakeSegment.nextdx;
+      snakeSegment.dy = snakeSegment.nextdy;
+      console.log(`x ${snakeSegment.x} y ${snakeSegment.y}`)
+    }
+    snakeSegment.x += snakeSegment.dx;
+    snakeSegment.y += snakeSegment.dy;
+  }
+
+  function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function updateIterFrames() {
+    clear();
+    drawSnakeSegment(snakeSegment.x, snakeSegment.y);
+    nextPosition();
+    requestAnimationFrame(updateIterFrames);
+  }
+
+  document.addEventListener("keydown", controlSnake);
+  // updateIterFrames();
 });
