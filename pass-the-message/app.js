aa260c143d1f8998697331deeae3f03ac8154069
mpampolina <miguel.r.pampolina@gmail.com>
2020-11-17 22:29:28 -0800

    add empty string checking

--- pass-the-message/app.js
+++ pass-the-message/app.js
@@ -3,7 +3,15 @@ function updateLastMessage(e) {
   e.preventDefault();
   const submitMsg = document.getElementById("submitMsg");
   const lastMsg = document.getElementById("lastMsg");
-  lastMsg.innerText = submitMsg.value;
+  const requiredField = document.getElementsByClassName("requiredField")[0]
+  if (submitMsg.value == '') {
+    requiredField.classList.remove("hidden")
+    /* add the hidden class after 3 seconds */
+    setTimeout( () => requiredField.classList.add("hidden"), 3000);
+  } else {
+    lastMsg.innerText = submitMsg.value;
+    submitMsg.value = null;
+  }
 }
 
 const lastMsgForm = document.getElementById("lastMsgForm");
